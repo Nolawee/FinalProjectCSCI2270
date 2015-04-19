b50e95b774087b338641e8617ad85767a32ab7a9
Nolawee Mengist <nome9478@colorado.edu>
2015-04-19 12:13:25 -0600

    Initial Commit

---
+++ FinalProjectCSCI2270/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FinalProjectCSCI2270
+//
+//  Created by Nolawee Mengist on 4/19/15.
+//  Copyright (c) 2015 nope. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
