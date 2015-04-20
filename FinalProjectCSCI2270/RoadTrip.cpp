e5914fb586b8dd8da884bd042963291ab7afdb6b
Nolawee Mengist <nome9478@colorado.edu>
2015-04-20 16:14:42 -0600

    RoadTrip.cpp and RoadTrip.h added

---
+++ FinalProjectCSCI2270/RoadTrip.cpp
@@ -0,0 +1,9 @@
+//
+//  RoadTrip.cpp
+//  FinalProjectCSCI2270
+//
+//  Created by Nolawee Mengist on 4/20/15.
+//  Copyright (c) 2015 nope. All rights reserved.
+//
+
+#include "RoadTrip.h"
---
+++ FinalProjectCSCI2270/RoadTrip.h
@@ -0,0 +1,14 @@
+//
+//  RoadTrip.h
+//  FinalProjectCSCI2270
+//
+//  Created by Nolawee Mengist on 4/20/15.
+//  Copyright (c) 2015 nope. All rights reserved.
+//
+
+#ifndef __FinalProjectCSCI2270__RoadTrip__
+#define __FinalProjectCSCI2270__RoadTrip__
+
+#include <stdio.h>
+
+#endif /* defined(__FinalProjectCSCI2270__RoadTrip__) */
